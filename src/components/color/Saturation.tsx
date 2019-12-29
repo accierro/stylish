a54c8149f598f3d2dede618470ab60976878c8f0
Vladislavs Murzjonks <accierro@gmail.com>
2019-12-29 15:52:00 +0000

    Finish Color Picker

--- src/components/color/Saturation.tsx
+++ src/components/color/Saturation.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useRef } from "react";
 import ColorPickerContext from "../../context/ColorPickerContext";
 import convert from "color-convert";
 
@@ -9,6 +9,7 @@ function isReactMouseEvent(
 }
 
 const Saturation: React.FC = () => {
+  const ref = useRef<HTMLDivElement>(null);
   const { color, setColor } = useContext(ColorPickerContext);
 
   function handleChange(
@@ -20,7 +21,36 @@ const Saturation: React.FC = () => {
       const c = convert.hsv.hsl([color.h, Math.ceil((x / 256) * 100), 100 - y]);
       setColor({ h: c[0], s: c[1], l: c[2] });
     } else {
-      console.log("Handle change");
+      const x = e.x;
+      const y = e.y;
+      if (ref.current) {
+        const {
+          width: containerWidth,
+          height: containerHeight
+        } = ref.current.getBoundingClientRect();
+        let left = x - ref.current.getBoundingClientRect().left;
+        let top = y - ref.current.getBoundingClientRect().top;
+
+        if (left < 0) {
+          left = 0;
+        }
+        if (left > containerWidth) {
+          left = containerWidth;
+        }
+        if (top < 0) {
+          top = 0;
+        }
+        if (top > containerHeight) {
+          top = containerHeight;
+        }
+
+        const c = convert.hsv.hsl([
+          color.h,
+          Math.ceil((left / 256) * 100),
+          100 - top
+        ]);
+        setColor({ h: c[0], s: c[1], l: c[2] });
+      }
     }
   }
 
@@ -29,8 +59,10 @@ const Saturation: React.FC = () => {
     window.removeEventListener("mouseup", onMouseUp);
   }
 
+  const hsv = convert.hsl.hsv([color.h, color.s, color.l]);
   return (
     <div
+      ref={ref}
       style={{
         width: "255px",
         height: "100px",
@@ -54,10 +86,26 @@ const Saturation: React.FC = () => {
           style={{
             width: "100%",
             height: "100%",
+            position: "relative",
+            overflow: "hidden",
             background:
               "linear-gradient(to top, rgb(0, 0, 0), rgba(0, 0, 0, 0))"
           }}
-        ></div>
+        >
+          <div
+            style={{
+              width: "10px",
+              height: "10px",
+              border: `1px solid ${color.l > 50 ? "black" : "white"}`,
+              position: "absolute",
+              transform: "translate(-5px, -5px)",
+              pointerEvents: "none",
+              left: hsv[1] + "%",
+              top: 100 - hsv[2] + "%",
+              borderRadius: "50%"
+            }}
+          ></div>
+        </div>
       </div>
     </div>
   );
